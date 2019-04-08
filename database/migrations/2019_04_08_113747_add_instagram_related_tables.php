7a737c9e7f22096a21ad8380ff9b9ceee22ac1f6
Martin Henriksen <martin.henriksen.92@gmail.com>
2019-04-08 13:43:33 +0200

    add migration for instagram statistics

---
+++ database/migrations/2019_04_08_113747_add_instagram_related_tables.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddInstagramRelatedTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('instagram_users', function(Blueprint $table) {
+            $table->integer('id', true, true);
+            $table->string('handle');
+            $table->timestamps();
+        });
+
+        Schema::create('instagram_user_statistics', function(Blueprint $table) {
+            $table->integer('id', true, true);
+            $table->integer('instagram_user_id')->unsigned();
+            $table->foreign('instagram_user_id')
+                ->references('id')->on('instagram_users');
+
+            $table->date('date');
+            $table->unsignedInteger('followers');
+            $table->unsignedInteger('followed_by');
+            $table->unsignedInteger('posts');
+
+            $table->timestamps();
+
+            $table->primary(array('instagram_user_id', 'date'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('instagram_user_statistics');
+        Schema::dropIfExists('instagram_users');
+    }
+}
